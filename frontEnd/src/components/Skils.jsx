6ae8c7dba7471a9745979876c66f5dc26e68479b
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-19 22:07:49 +0300

    skills

--- frontEnd/src/components/Skils.jsx
+++ frontEnd/src/components/Skils.jsx
@@ -4,32 +4,43 @@ import react from '../assets/react.png'
 
 function Skils() {
   return (
-    <div>
+    <div className='py-16 bg-slate-500'>
+        <div className='max-w-[1240px] mx-auto'>
    
-   <h1 className='text-center font-bold text-2xl'> My Skills</h1>
+   <h1 className='text-center font-bold text-2xl py-6 my-4'> My Skills</h1>
    <div className='grid grid-cols-4'>
     <div className='flex flex-col items-center'>
-        <img src={html} alt="" className='w-[80]  '/>
-        <p className='my-3'>HTML</p>
+        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <img src={html} alt="" className=' p-4 ' />
+        </div>
+        <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
     </div>
 
     <div className='flex flex-col items-center'>
-        <img src={html} alt="" className='w-[80]  '/>
-        <p className='my-3'>HTML</p>
+        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <img src={react} alt="" className=' p-2 ' />
+        </div>
+        <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
     </div>
 
     <div className='flex flex-col items-center'>
-        <img src={react} alt="" className='w-[80]  '/>
-        <p className='my-3'>REACT</p>
+        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <img src={html} alt="" className=' p-2' />
+        </div>
+        <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
     </div>
 
+
     <div className='flex flex-col items-center'>
-        <img src={html} alt="" className='w-[80]  '/>
-        <p className='my-3'>HTML</p>
+        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <img src={react} alt="" className=' p-2 ' />
+        </div>
+        <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
     </div>
+
    </div>
 
-
+   </div>
     </div>
   )
 }
