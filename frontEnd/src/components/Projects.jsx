b60d386d17984213942a640a6d1844398417b524
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-19 22:08:42 +0300

    projects

--- frontEnd/src/components/Projects.jsx
+++ frontEnd/src/components/Projects.jsx
@@ -1,13 +1,63 @@
 import React from 'react'
+import works from '../assets/works.png'
+import { FaGithub } from "react-icons/fa";
 
 function Projects() {
   return (
-    <div>
-        <h1 className='text-center text-2xl'>My Projects</h1>
-        <div>
-            <div>
-                
+    <div className='w-full bg-orange-200 py-16'>
+        <div className='max-w-[1240px] mx-auto' >
+        <h1 className='text-center text-3xl'>My Projects</h1>
+        <div className='grid grid-cols-3 gap-8'>
+            <div className='w-full shadow-xl py-8'>
+                <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
+                <p className='p-2'>Create stunning text effects with our font 
+                    style text effect generator. Choose from a variety 
+                    of fonts and effortlessly transform your text into a 
+                    3D masterpiece. Add texture and depth to your words, 
+                    whether you want to evoke the wildness of a
+                     leopard or the charm of a zoo full of animals.</p>
+                     <img src={works} alt="" className='p-4' />
+                     <div className='flex items-center justify-center'>
+                     <button className='flex p-2 mx-2 items-center bg-slate-400 rounded-lg'> <FaGithub/>Repo</button>
+                     <button className='bg-slate-400 p-2 rounded-lg'>Live</button>
+                     </div>
+                   
+            
             </div>
+
+            <div className='w-full shadow-xl py-8'>
+                <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
+                <p className='p-2'>Create stunning text effects with our font 
+                    style text effect generator. Choose from a variety 
+                    of fonts and effortlessly transform your text into a 
+                    3D masterpiece. Add texture and depth to your words, 
+                    whether you want to evoke the wildness of a
+                     leopard or the charm of a zoo full of animals.</p>
+                     <img src={works} alt="" className='p-4' />
+                     <div className='flex items-center justify-center'>
+                     <button className='flex p-2 mx-2 items-center bg-slate-400 rounded-lg'> <FaGithub/>Repo</button>
+                     <button className='bg-slate-400 p-2 rounded-lg'>Live</button>
+                     </div>
+                   
+            
+            </div>
+            <div className='w-full shadow-xl py-8'>
+                <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
+                <p className='p-2'>Create stunning text effects with our font 
+                    style text effect generator. Choose from a variety 
+                    of fonts and effortlessly transform your text into a 
+                    3D masterpiece. Add texture and depth to your words, 
+                    whether you want to evoke the wildness of a
+                     leopard or the charm of a zoo full of animals.</p>
+                     <img src={works} alt="" className='p-4' />
+                     <div className='flex items-center justify-center'>
+                     <button className='flex p-2 mx-2 items-center bg-slate-400 rounded-lg'> <FaGithub/>Repo</button>
+                     <button className='bg-slate-400 p-2 rounded-lg'>Live</button>
+                     </div>
+                   
+            
+            </div>
+        </div>
         </div>
 
     </div>
