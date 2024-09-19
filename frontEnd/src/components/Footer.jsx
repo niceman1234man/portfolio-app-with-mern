8b6952eda7c1f5005d748f45e8956422cb8c0084
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-19 22:12:20 +0300

    footer

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -3,37 +3,39 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>
-        <div className='grid grid-cols-4 mx-4'>
-            <div>
-                <h1 className='text-2xl font-bold'>About</h1>
+    <div className=' py-16 mx-auto bg-slate-500'>
+        <div className='max-w-[1240px] mx-auto'>
+        <div className='grid grid-cols-4 mx-4 gap-8'>
+            <div className='p-2'>
+                <h1 className='text-2xl font-bold mb-3'>About</h1>
                 <p>I'm full stack developer .I have done a lot of projects.I got invaluable experience from my work.</p>
             </div>
-            <div>
-                <h1 className='text-2xl font-bold'>Links</h1>
+            <div className='p-2'>
+                <h1 className='text-2xl font-bold mb-3'>Links</h1>
                 <ul>
-                    <li className='flex items-center '><FaLongArrowAltRight/>Home</li>
-                    <li className='flex items-center'> <FaLongArrowAltRight/>About</li>
-                    <li className='flex items-center'> <FaLongArrowAltRight/>services</li>
-                    <li className='flex items-center'> <FaLongArrowAltRight/>skills</li>
-                    <li className='flex items-center'> <FaLongArrowAltRight/>Projects</li>
-                    <li className='flex items-center'> <FaLongArrowAltRight/>Contacts</li>
+                    <li className='flex items-center '>Home</li>
+                    <li className='flex items-center'> About</li>
+                    <li className='flex items-center'> services</li>
+                    <li className='flex items-center'> skills</li>
+                    <li className='flex items-center'> Projects</li>
+                    <li className='flex items-center'> Contacts</li>
                 </ul>
             </div>
-            <div>
-                <h1 className='text-2xl font-bold'>Services</h1>
+            <div className='p-2'>
+                <h1 className='text-2xl font-bold mb-3'>Services</h1>
                 <p>Full stack Development</p>
             </div>
-            <div>
-                <h1 className='text-2xl font-bold'> Have a Question?</h1>
+            <div className='p-2'>
+                <h1 className='text-2xl font-bold mb-3'> Have a Question?</h1>
                 <p>+251954121503</p>
                 <p>niceman1234man</p>
                 <p>yihunie</p>
             </div>
             <div>
-                <h1></h1>
+                <h1></h1> 
             </div>
 
+        </div>
         </div>
     </div>
   )
