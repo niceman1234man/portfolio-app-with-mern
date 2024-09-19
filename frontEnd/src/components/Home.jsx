4e112b9b512b8d29a474f3bd2969d28170cd399b
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-19 22:09:31 +0300

    home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import me2 from '../assets/me2.jpg'
 
 function Home() {
   return (
-    <div className=''>
-        <div className='mx-4 grid grid-cols-2'>
+    <div className='bg-red-300 w-full py-16 mt-0'>
+        <div className='max-w-[1240px] mx-auto'>
+        <div className='mx-4 grid grid-cols-2 gap-8'>
         <div className=''>
-        <h2 className='text-[20px] p-2'>Hello!</h2>
+        <h2 className='text-[30px] p-2'>Hello!</h2>
         <p className='text-5xl'>I'm Yihunie Tarekegn Full satck Developer</p>
-        <button className='bg-yellow-400 p-4 rounded-full text-xl my-3'>My Works</button>
+        <button className='bg-yellow-400 p-4 rounded-full text-xl my-8'>My Works</button>
         </div>
-<div className=''>
-    <img src="" alt="" />
+<div className='w-[90%]   p-4  flex items-center justify-center rounded-lg mx-auto'>
+    <img src={me2} alt="" className='w-[300px] rounded-full  border-8 border-fuchsia-950  ' />
+</div>
 </div>
 </div>
     </div>
