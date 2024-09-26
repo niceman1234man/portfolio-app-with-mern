839b97a88150c3279dc34fc650ee6b2a2a349668
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-26 09:28:22 +0300

    footer

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { FaLongArrowAltRight } from "react-icons/fa";
+import linkedin from '../assets/linkedin.png'
 
 function Footer() {
   return (
     <div className=' py-16 mx-auto bg-slate-500'>
         <div className='max-w-[1240px] mx-auto'>
-        <div className='grid grid-cols-4 mx-4 gap-8'>
-            <div className='p-2'>
+        <div className='grid lg:grid-cols-5  mx-4 gap-8'>
+            <div className='p-2 col-span-2'>
                 <h1 className='text-2xl font-bold mb-3'>About</h1>
                 <p>I'm full stack developer .I have done a lot of projects.I got invaluable experience from my work.</p>
+                <div className='grid grid-cols-5 gap-4 my-2 py-2'>
+                    <img src={linkedin} alt="" className='w-[30px]'/>
+                    <img src={linkedin} alt="" className='w-[30px]'/>
+                    <img src={linkedin} alt="" className='w-[30px]'/>
+                </div>
             </div>
             <div className='p-2'>
                 <h1 className='text-2xl font-bold mb-3'>Links</h1>
