3e212a8ff83feebc4f2a81751b2342193d793947
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-26 09:26:16 +0300

    skills

--- frontEnd/src/components/Skils.jsx
+++ frontEnd/src/components/Skils.jsx
@@ -8,23 +8,23 @@ function Skils() {
         <div className='max-w-[1240px] mx-auto'>
    
    <h1 className='text-center font-bold text-2xl py-6 my-4'> My Skills</h1>
-   <div className='grid grid-cols-4'>
+   <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
     <div className='flex flex-col items-center'>
-        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <div className='bg-white p-4 rounded-full w-[100px] md:w-[120px]'>
         <img src={html} alt="" className=' p-4 ' />
         </div>
         <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
     </div>
 
     <div className='flex flex-col items-center'>
-        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <div className='bg-white p-4 rounded-full w-[100px] md:w-[120px]'>
         <img src={react} alt="" className=' p-2 ' />
         </div>
         <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
     </div>
 
     <div className='flex flex-col items-center'>
-        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <div className='bg-white p-4 rounded-full w-[100px] md:w-[120px]'>
         <img src={html} alt="" className=' p-2' />
         </div>
         <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
@@ -32,7 +32,7 @@ function Skils() {
 
 
     <div className='flex flex-col items-center'>
-        <div className='bg-white p-4 rounded-full w-[120px]'>
+        <div className='bg-white p-4 rounded-full w-[100px] md:w-[120px]'>
         <img src={react} alt="" className=' p-2 ' />
         </div>
         <p className='my-3 bg-orange-500 w-32 text-center p-2 rounded-full font-bold'>HTML</p>
