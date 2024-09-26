47a926eec573649ceb29622519a293c07f1c0798
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-26 09:27:46 +0300

    navbar

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+import header from '../assets/header.png';
+
 
 function Navbar() {
+  const  [nav,setNav]=useState(false);
+  const HandleNav=()=>{
+    setNav(!nav);
+  }
+
   return (
-    <div className=' h-24 w-full bg-slate-400 bg-fixed'>
-        <div className='max-w-[1240px] mx-auto'>
-        <div className='fixed flex justify-between mx-4 items-center'>
-        <div className='mr-10'>
-         YIHUNIE
+    <div 
+      className="md:h-24 w-full bg-fixed bg-cover h-[250px]" 
+      style={{ backgroundImage: `url(${header})` }}
+    >
+      <div className="max-w-[1240px] mx-auto h-full">
+        <div className="flex justify-between items-center h-full px-4">
+          <div className="mr-10 text-white font-serif">
+            YIHUNIE
+          </div>
+          <div className="font-bold text-white flex flex-col justify-center items-center">
+            <ul className={`md:flex sm:w-[30%] ${nav? 'block' : 'hidden'} space-x-3 items-center justify-center`}>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ml-3 ">Home</li>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ">About</li>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ">Resume</li>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ">Services</li>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ">Skills</li>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ">Projects</li>
+              <li className="hover:border-b-2 border-yellow-400 cursor-pointer ">Contact</li>
+            </ul>
+            <div  className='flex absolute top-2 right-4 md:hidden w-[20px]' onClick={HandleNav}>
+              {nav? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}</div>
+          </div>
         </div>
-        <div className='ml-96 font-bold'>
-         <ul className='flex '>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>Home</li>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>About</li>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>Resume</li>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>Services</li>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>Skills</li>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>Projects</li>
-            <li className='m-4 hover:border-b-2 border-y-fuchsia-950'>Contact</li>
-         </ul>
-        </div>
-        </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
