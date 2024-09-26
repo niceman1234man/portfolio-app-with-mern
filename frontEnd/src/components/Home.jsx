6e13c332bd9cf539ec6833953bbed638748d1006
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-26 09:28:59 +0300

    Home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+
 import me2 from '../assets/me2.jpg'
 
 function Home() {
   return (
-    <div className='bg-red-300 w-full py-16 mt-0'>
+    <div className='bg-slate-800 w-full py-16 mt-0'>
         <div className='max-w-[1240px] mx-auto'>
-        <div className='mx-4 grid grid-cols-2 gap-8'>
-        <div className=''>
+        <div className='mx-4 grid md:grid-cols-2 gap-8'>
+        <div className='text-white'>
         <h2 className='text-[30px] p-2'>Hello!</h2>
-        <p className='text-5xl'>I'm Yihunie Tarekegn Full satck Developer</p>
-        <button className='bg-yellow-400 p-4 rounded-full text-xl my-8'>My Works</button>
+        <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif '> <Typewriter words={[ "I'm Yihunie Tarekegn"," Full stack Developer"]}
+            typeSpeed={120}
+            backSpeed={130}
+            loop/></p>
+            <div className='flex items-center justify-center mt-6 mx-auto'>
+        <button className='bg-sky-700 p-4 rounded-full text-xl my-8 font-semibold font-sans'>My Works</button>
+        </div>
         </div>
 <div className='w-[90%]   p-4  flex items-center justify-center rounded-lg mx-auto'>
-    <img src={me2} alt="" className='w-[300px] rounded-full  border-8 border-fuchsia-950  ' />
+    <img src={me2} alt="" className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] rounded-full  border-8 border-fuchsia-950   ' />
 </div>
 </div>
 </div>
