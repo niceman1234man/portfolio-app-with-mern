aa4b8b242863ce6dd55769e9506d706b0fb7b69d
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-28 12:50:32 +0300

    Services

---
+++ frontEnd/src/components/Service.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Service() {
+  return (
+    <div className='w-full bg-slate-500 py-16 text-white font-semibold'>
+    <div className='max-w-[1240px] mx-auto' >
+    <h1 className='text-center text-3xl'>My Services</h1>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        <div className='w-full shadow-xl py-8'>
+            <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
+            <p className='p-2'>Create stunning text effects with our font 
+                style text effect generator. Choose from a variety 
+                of fonts and effortlessly transform your text into a 
+                3D masterpiece. Add texture and depth to your words, 
+                whether you want to evoke the wildness of a
+                 leopard or the charm of a zoo full of animals.</p>
+                 
+               
+        
+        </div>
+
+        <div className='w-full shadow-xl py-8'>
+            <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
+            <p className='p-2'>Create stunning text effects with our font 
+                style text effect generator. Choose from a variety 
+                of fonts and effortlessly transform your text into a 
+                3D masterpiece. Add texture and depth to your words, 
+                whether you want to evoke the wildness of a
+                 leopard or the charm of a zoo full of animals.</p>
+                
+               
+        
+        </div>
+        <div className='w-full shadow-xl py-8'>
+            <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
+            <p className='p-2'>Create stunning text effects with our font 
+                style text effect generator. Choose from a variety 
+                of fonts and effortlessly transform your text into a 
+                3D masterpiece. Add texture and depth to your words, 
+                whether you want to evoke the wildness of a
+                 leopard or the charm of a zoo full of animals.</p>
+                
+               
+        
+        </div>
+    </div>
+    </div>
+
+</div>
+  )
+}
+
+export default Service
