ac4c5b167c7e3bfad03ed5a3e06474273f038abf
niceman1234man <yihunietarekegn18@gmail.com>
2024-09-28 12:43:34 +0300

    Home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -5,9 +5,8 @@ import me2 from '../assets/me2.jpg'
 
 function Home() {
   return (
-    <div className='bg-slate-800 w-full py-16 mt-0'>
-        <div className='max-w-[1240px] mx-auto'>
-        <div className='mx-4 grid md:grid-cols-2 gap-8'>
+    <div className='bg-black w-full py-16 mt-0 h-screen'>
+        <div className='max-w-[1240px] mx-auto flex items-center justify-center'>
         <div className='text-white'>
         <h2 className='text-[30px] p-2'>Hello!</h2>
         <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif '> <Typewriter words={[ "I'm Yihunie Tarekegn"," Full stack Developer"]}
@@ -15,15 +14,13 @@ function Home() {
             backSpeed={130}
             loop/></p>
             <div className='flex items-center justify-center mt-6 mx-auto'>
-        <button className='bg-sky-700 p-4 rounded-full text-xl my-8 font-semibold font-sans'>My Works</button>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black'>My Works</button>
         </div>
         </div>
-<div className='w-[90%]   p-4  flex items-center justify-center rounded-lg mx-auto'>
-    <img src={me2} alt="" className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] rounded-full  border-8 border-fuchsia-950   ' />
+
 </div>
 </div>
-</div>
-    </div>
+   
   )
 }
 
