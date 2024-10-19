05d4d5506d84529da0e8777e6dc74dee2e7bc8d6
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-19 14:38:42 -0700

    modified app

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,14 +5,18 @@ import Footer from './components/Footer'
 import Contact from './components/Contact'
 import Skils from './components/Skils'
 import Projects from './components/Projects'
+import About from './components/About'
+import Service from './components/Service'
 
 function App() {
   return (
    <>
    <Navbar/>
    <Home/>
+   <About/>
    <Skils/>
    <Projects/>
+   <Service/>
    <Contact/>
    <Footer/>
 
