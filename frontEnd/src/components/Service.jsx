980ac710ad062aded5a153e6bb796a79b435cf07
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-19 14:42:09 -0700

    modified Services

--- frontEnd/src/components/Service.jsx
+++ frontEnd/src/components/Service.jsx
@@ -6,7 +6,7 @@ function Service() {
     <div className='max-w-[1240px] mx-auto' >
     <h1 className='text-center text-3xl'>My Services</h1>
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        <div className='w-full shadow-xl py-8'>
+        <div className='w-full shadow-xl py-8 px-2'>
             <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
             <p className='p-2'>Create stunning text effects with our font 
                 style text effect generator. Choose from a variety 
@@ -19,7 +19,7 @@ function Service() {
         
         </div>
 
-        <div className='w-full shadow-xl py-8'>
+        <div className='w-full shadow-xl py-8 px-2'>
             <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
             <p className='p-2'>Create stunning text effects with our font 
                 style text effect generator. Choose from a variety 
@@ -31,7 +31,7 @@ function Service() {
                
         
         </div>
-        <div className='w-full shadow-xl py-8'>
+        <div className='w-full shadow-xl py-8 px-2'>
             <h1 className='text-center font-bold text-2xl py-4'>Title</h1>
             <p className='p-2'>Create stunning text effects with our font 
                 style text effect generator. Choose from a variety 
