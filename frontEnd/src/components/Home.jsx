f1e1c93f40db4b963cc7e0bab48b6b8e055bd699
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-20 19:08:14 -0700

    modified Home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -6,21 +6,21 @@ import me2 from '../assets/me2.jpg'
 function Home() {
   return (
     <div className='bg-black w-full py-16 mt-0 h-screen'>
-        <div className='max-w-[1240px] mx-auto flex items-center justify-center'>
+      <div className='max-w-[1240px] mx-auto flex items-center justify-center'>
         <div className='text-white'>
-        <h2 className='text-[30px] p-2'>Hello!</h2>
-        <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif '> <Typewriter words={[ "I'm Yihunie Tarekegn"," Full stack Developer"]}
+          <h2 className='text-[30px] p-2'>Hello!</h2>
+          <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif '> <Typewriter words={["I'm Yihunie Tarekegn", " Full stack Developer"]}
             typeSpeed={120}
             backSpeed={130}
-            loop/></p>
-            <div className='flex items-center justify-center mt-6 mx-auto'>
-        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black'>My Works</button>
-        </div>
+            loop /></p>
+          <div className='flex items-center justify-center mt-6 mx-auto'>
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black'>My Works</button>
+          </div>
         </div>
 
-</div>
-</div>
-   
+      </div>
+    </div>
+
   )
 }
 
