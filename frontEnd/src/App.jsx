31f655fbd500e09476e37f58000128c4a32829ae
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-20 19:07:08 -0700

    modified app

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
-import Contact from './components/Contact'
-import Skils from './components/Skils'
-import Projects from './components/Projects'
-import About from './components/About'
-import Service from './components/Service'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Skils from './components/Skils'; 
+import Projects from './components/Projects';
+import About from './components/About';
+import Service from './components/Service';
+import Information from './components/Input/Information';
 
 function App() {
   return (
-   <>
-   <Navbar/>
-   <Home/>
-   <About/>
-   <Skils/>
-   <Projects/>
-   <Service/>
-   <Contact/>
-   <Footer/>
-
-   </>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={
+          <div>
+            <Home />
+            <About />
+            <Skils />
+            <Projects />
+            <Service />
+            <Contact />
+            <Footer />
+          </div>
+        } />
+        <Route path='/add-info' element={<Information />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
