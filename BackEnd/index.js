2a705c93e7b849d6c05d7fb789909cfa566f358c
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-22 22:02:36 -0700

    index

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,20 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import {connectDb} from './config/db.js'
+import router from './routes/router.js'
+import cors from 'cors'
 dotenv.config();
-
-const port = process.env.PORT ||9000 ; // Default to 9000 if PORT is not set
-
+const port = process.env.PORT || 9000 ; // Default to 9000 if PORT is not set
 const app = express();
-
-app.get('/hi', (req, res) => {
-    res.send("hello express nnnnnn");
-});
-
+app.use(cors());
+app.use(express.json());
+app.use('/home',router);
 app.listen(port, (err) => {
     if (err) {
         console.error("Failed to start server:", err);
     } else {
+        connectDb();
         console.log(`Server is running on http://localhost:${port}`);
     }
 });
