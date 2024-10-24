1ef847743a156c3b2a94c22ef4a6995ee782b8f6
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-23 22:34:52 -0700

    models

--- BackEnd/models/index.js
+++ BackEnd/models/index.js
@@ -1,48 +1,57 @@
 import mongoose from "mongoose";
-const home= new  mongoose.Schema({
- greating :{
-    type:String
- },
- title:{
-    type:String
- }
-},{timestamps:true});
-const Home =mongoose.model('Home',home);
-
-
-const about =new mongoose.Schema({
-    photo:{
-        type:String
+const home = new mongoose.Schema(
+  {
+    greating: {
+      type: String,
     },
-    description:{
-        type:String
-    }
-},{timestamps:true});
-
-const About=mongoose.model('About',about);
-
-const skills=new mongoose.Schema({
-    photo:{
-        type:String
+    title: {
+      type: String,
     },
-    name:{
-        type:String
-    }
-},{timestamps:true});
+  },
+  { timestamps: true }
+);
+const Home = mongoose.model("Home", home);
 
-const Skill=mongoose.model('Skill',skills);
-
-const sevice=mongoose.Schema({
-    name:{
-        type:String
+const about = new mongoose.Schema(
+  {
+    photo: {
+      type: String,
     },
-    desc:{
-        type:String
-    }
-},{timestamps:true});
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-const Service=mongoose.model('Service',sevice);
+const About = mongoose.model("About", about);
 
+const skills = new mongoose.Schema(
+  {
+    photo: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
+const Skill = mongoose.model("Skill", skills);
 
-export default {Home,About,Skill,Service};
+const sevice = mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Service = mongoose.model("Service", sevice);
+
+export default { Home, About, Skill, Service };
