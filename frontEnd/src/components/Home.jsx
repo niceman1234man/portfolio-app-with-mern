7aa6afb88db74e2e6a2dece9c9c64d73011e2458
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-26 10:19:24 -0700

    home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { Typewriter } from 'react-simple-typewriter'
-
-import me2 from '../assets/me2.jpg'
+import React, { useState, useEffect } from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import axios from 'axios';
 
 function Home() {
+  const [home, setHome] = useState({ greeting: "Hi!", title: "Full Stack Developer" });
+
+  useEffect(() => {
+    axios.get('http://localhost:9000')
+      .then((result) => {
+        setHome(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []); // Empty array to run only once on mount
+
   return (
     <div className='bg-black w-full py-16 mt-0 h-screen'>
       <div className='max-w-[1240px] mx-auto flex items-center justify-center'>
         <div className='text-white'>
-          <h2 className='text-[30px] p-2'>Hello!</h2>
-          <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif '> <Typewriter words={["I'm Yihunie Tarekegn", " Full stack Developer"]}
-            typeSpeed={120}
-            backSpeed={130}
-            loop /></p>
+          <h2 className='text-[30px] p-2'>{home.greeting}</h2>
+          <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif'>
+            <Typewriter
+              words={["I'm Yihunie Tarekegn", home.title]}
+              typeSpeed={120}
+              backSpeed={130}
+              loop
+            />
+          </p>
           <div className='flex items-center justify-center mt-6 mx-auto'>
-            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black'>My Works</button>
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black'>
+              My Works
+            </button>
           </div>
         </div>
-
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
