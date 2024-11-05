9738deedfd71ca9c5f6c733bd5eab6d4e5d7a08a
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-04 20:56:40 -0800

    home

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Typewriter } from 'react-simple-typewriter';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Typewriter } from "react-simple-typewriter";
+import axios from "axios";
 
 function Home() {
-  const [home, setHome] = useState({ greeting: "Hi!", title: "Full Stack Developer" });
+  const [home, setHome] = useState({
+    greeting: "Hi!",
+    title: "Full Stack Developer",
+  });
 
   useEffect(() => {
-    axios.get('http://localhost:9000/home/h')
+    axios
+      .get("http://localhost:9000/home/h")
       .then((result) => {
         console.log(result);
         setHome(result.data.data[0]);
@@ -17,11 +21,11 @@ function Home() {
   }, []);
 
   return (
-    <div className='bg-black w-full py-16 mt-0 h-screen'>
-      <div className='max-w-[1240px] mx-auto flex items-center justify-center'>
-        <div className='text-white'>
-          <h2 className='text-[30px] p-2 font-semibold'>{home.greeting}!</h2>
-          <p className='text-3xl font-semibold md:text-4xl lg:text-5xl font-serif'>
+    <div className="bg-black w-full py-16 mt-0 h-screen">
+      <div className="max-w-[1240px] mx-auto flex items-center justify-center">
+        <div className="text-white">
+          <h2 className="text-[30px] p-2 font-semibold">{home.greeting}!</h2>
+          <p className="text-3xl font-semibold md:text-4xl lg:text-5xl font-serif">
             <Typewriter
               words={["I'm Yihunie Tarekegn", home.title]}
               typeSpeed={120}
@@ -29,10 +33,11 @@ function Home() {
               loop
             />
           </p>
-          <div className='flex items-center justify-center mt-6 mx-auto'>
-            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black'><a href="https://github.com/niceman1234man?tab=repositories">
-
-              My Works</a>
+          <div className="flex items-center justify-center mt-6 mx-auto">
+            <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 py-2 mx-auto text-black">
+              <a href="https://github.com/niceman1234man?tab=repositories">
+                My Works
+              </a>
             </button>
           </div>
         </div>
